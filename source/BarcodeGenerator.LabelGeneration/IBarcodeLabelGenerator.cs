b67be8e96a9996c3b45ee4f27ab3264f4073c4a0
Robert <Robert.Eberhart@gmail.com>
2026-06-10 12:22:12 -0400

    Add IBarcodeLabelGenerator interface to BarcodeGenerator.LabelGeneration: - Defines contract for barcode label generation.

---
+++ source/BarcodeGenerator.LabelGeneration/IBarcodeLabelGenerator.cs
@@ -0,0 +1,17 @@
+﻿// ***********************************************************************
+// Assembly          : ${$NAMESPACE$}
+// Author            : Robert Eberhart
+// Created           : 06-10-2026
+// ***********************************************************************
+// <copyright file="IBarcodeLabelGenerator.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using BarcodeGenerator.Entities;
+
+namespace BarcodeGenerator.LabelGeneration;
+
+public interface IBarcodeLabelGenerator {
+    IList<BarcodeLabel> Generate(int startIndex, int endIndex, string sourceCode, DateTime datePurchased);
+}
