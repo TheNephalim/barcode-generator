8fe403af5651f4c01d09f2f318678d9e459566ac
Robert <Robert.Eberhart@gmail.com>
2026-06-10 12:21:31 -0400

    Refactor Program.cs: - Integrated Autofac for dependency injection. - Registered modules and forms for the application.

--- source/BarcodeGenerator/Program.cs
+++ source/BarcodeGenerator/Program.cs
@@ -1,17 +1,34 @@
-namespace BarcodeGenerator
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
-        }
+using Autofac;
+
+namespace BarcodeGenerator;
+
+/// <summary>
+/// Represents the entry point for the BarcodeGenerator application.
+/// </summary>
+/// <remarks>
+/// This class contains the main logic to initialize and run the application.
+/// </remarks>
+internal static class Program {
+
+    /// <summary>
+    /// Serves as the main entry point for the BarcodeGenerator application.
+    /// </summary>
+    /// <remarks>
+    /// This method initializes the application configuration and starts the main form of the BarcodeGenerator application.
+    /// </remarks>
+    [STAThread]
+    private static void Main() {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+
+        var builder = new ContainerBuilder();
+
+        ModuleRegistrar.Register(builder);
+        FormRegistrar.Register(builder);
+
+        using var container = builder.Build();
+
+        Application.Run(container.Resolve<MainForm>());
     }
 }
