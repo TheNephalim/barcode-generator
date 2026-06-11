e4c39ddf494206a3fac82a8d6cc4954a5ad6057d
Robert <Robert.Eberhart@gmail.com>
2026-06-11 15:26:29 -0400

    Add LabelTemplateType enumeration - Defined an enumeration to represent different label template types (e.g., OneByThree).

---
+++ source/BarcodeGenerator.Entities/LabelTemplateType.cs
@@ -0,0 +1,21 @@
+﻿// ***********************************************************************
+// Assembly          : ${$NAMESPACE$}
+// Author            : Robert Eberhart
+// Created           : 06-11-2026
+// ***********************************************************************
+// <copyright file="LabelTemplateType.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+namespace BarcodeGenerator.Entities;
+
+/// <summary>
+/// Represents the types of label templates available for barcode generation.
+/// </summary>
+/// <summary>
+/// A label template with dimensions of 1x3.
+/// </summary>
+public enum LabelTemplateType {
+    OneByThree
+}
