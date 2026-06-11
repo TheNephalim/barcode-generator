c53e97cc5f6a3814d5834e6c567ff783d83486b0
Robert <Robert.Eberhart@gmail.com>
2026-06-11 15:26:54 -0400

    Implement LabelRendererFactory - Added a concrete implementation of `IRendererFactory` to create label renderers.

---
+++ source/BarcodeGenerator.LabelGeneration/LabelRendererFactory.cs
@@ -0,0 +1,24 @@
+﻿// ***********************************************************************
+// Assembly          : BarcodeGenerator.LabelGeneration
+// Author            : Robert Eberhart
+// Created           : 06-11-2026
+// ***********************************************************************
+// <copyright file="LabelRenderedFactory.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using Autofac.Features.Indexed;
+using BarcodeGenerator.Entities;
+
+namespace BarcodeGenerator.LabelGeneration;
+
+public class LabelRendererFactory : IRendererFactory {
+
+    public LabelRendererFactory(IIndex<LabelTemplateType, ILabelRenderer> _labelRenderers) {
+    }
+
+    public ILabelRenderer GetLabelRenderer(LabelTemplateType labelTemplateType) {
+        throw new NotImplementedException();
+    }
+}
