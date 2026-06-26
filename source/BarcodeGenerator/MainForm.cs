e6df0240664602d784637c95d1d396054aeb0b9c
Robert <Robert.Eberhart@gmail.com>
2026-06-26 08:49:36 -0400

    Refactor MainForm logic: - Replaced direct dependencies with factory methods for label generator forms. - Removed unused methods and streamlined event handlers.

--- source/BarcodeGenerator/MainForm.cs
+++ source/BarcodeGenerator/MainForm.cs
@@ -1,6 +1,3 @@
-using BarcodeGenerator.Entities;
-using BarcodeGenerator.LabelGeneration;
-
 namespace BarcodeGenerator;
 
 /// <summary>
@@ -11,21 +8,8 @@ namespace BarcodeGenerator;
 /// including input fields for source code, start number, and end number.
 /// </remarks>
 public partial class MainForm : Form {
-    private readonly IBarcodeLabelGenerator _barcodeLabelGenerator;
-    private readonly ILabelPrinterFactory _printerFactory;
-    private readonly IRenderedBarcodeLabelGenerator _renderedBarcodeLabelGenerator;
-    private readonly Dictionary<string, string> _sources = new() {
-        { "PC", "Personal Collection" },
-        { "GW", "Goodwill" },
-        { "TS", "Thrift Store" },
-        { "FB", "Facebook Marketplace" },
-        { "ES", "Estate Sale" },
-        { "LH", "Linda Hirschberg" },
-        { "ETA", "Early Times Auction" },
-        { "FM", "Fred Melby" },
-        { "JW", "Jim Wood" },
-        { "OT", "Other" }
-    };
+    private readonly Func<BarcodeLabelGenerator> _barcodeLabelGeneratorFactory;
+    private readonly Func<PriceLabelGenerator> _pricingLabelGeneratorFormFactory;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="MainForm"/> class.
@@ -34,116 +18,24 @@ public partial class MainForm : Form {
     /// This constructor sets up the main form of the Barcode Generator application
     /// by initializing its components and preparing the user interface for interaction.
     /// </remarks>
-    public MainForm(IRenderedBarcodeLabelGenerator renderedBarcodeLabelGenerator, IBarcodeLabelGenerator barcodeLabelGenerator, ILabelPrinterFactory printerFactory) {
+    public MainForm(
+        Func<BarcodeLabelGenerator> barcodeLabelGeneratorFactory,
+        Func<PriceLabelGenerator> pricingLabelGeneratorFormFactory
+        ) {
         InitializeComponent();
 
-        _renderedBarcodeLabelGenerator =
-            renderedBarcodeLabelGenerator ?? throw new ArgumentNullException(nameof(renderedBarcodeLabelGenerator));
-        _barcodeLabelGenerator = barcodeLabelGenerator ?? throw new ArgumentNullException(nameof(barcodeLabelGenerator));
-        _printerFactory = printerFactory ?? throw new ArgumentNullException(nameof(printerFactory));
+        _barcodeLabelGeneratorFactory = barcodeLabelGeneratorFactory ?? throw new ArgumentNullException(nameof(barcodeLabelGeneratorFactory));
+        _pricingLabelGeneratorFormFactory = pricingLabelGeneratorFormFactory ?? throw new ArgumentNullException(nameof(pricingLabelGeneratorFormFactory));
     }
 
-    /// <summary>
-    /// Validates the range of label indexes to ensure the start index is not greater than the end index.
-    /// </summary>
-    /// <param name="startIndex">The starting index of the label range.</param>
-    /// <param name="endIndex">The ending index of the label range.</param>
-    /// <returns>
-    /// <c>true</c> if the start index is greater than the end index, indicating an invalid range;
-    /// otherwise, <c>false</c>.
-    /// </returns>
-    /// <remarks>
-    /// Displays a warning message to the user if the range is invalid.
-    /// </remarks>
-    private static bool InvalidLabelIndexes(int startIndex, int endIndex) {
-        if (startIndex > endIndex) {
-            MessageBox.Show("Start number cannot be greater than end number",
-                "Invalid Range",
-                MessageBoxButtons.OK,
-                MessageBoxIcon.Warning);
-
-            return true;
-        }
-
-        return false;
+    private void btnLaunchBarcodeLabelGenerator_Click(object sender, EventArgs e) {
+        var form = _barcodeLabelGeneratorFactory();
+        form.ShowDialog(this);
     }
 
-    /// <summary>
-    /// Handles the click event of the <see cref="btnGenerate"/> button.
-    /// Generates barcode labels based on the specified start and end indexes, source code, and purchase date.
-    /// </summary>
-    /// <param name="sender">The source of the event, typically the <see cref="btnGenerate"/> button.</param>
-    /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
-    /// <remarks>
-    /// This method validates the input indexes and generates barcode labels if the validation passes.
-    /// A message box is displayed upon successful generation.
-    /// </remarks>
-    private void btnGenerate_Click(object sender, EventArgs e) {
-        var sourceCode = cmboSources.SelectedValue?.ToString();
-        var datePurchased = monthCalendar1.SelectionStart;
-
-        var startIndex = Convert.ToInt32(nudStartNumber.Value);
-        var endIndex = Convert.ToInt32(nudEndNumber.Value);
-
-        if (InvalidLabelIndexes(startIndex, endIndex)) return;
-
-        if (string.IsNullOrEmpty(sourceCode)) {
-            MessageBox.Show("Please select a source code.",
-                "Missing Source",
-                MessageBoxButtons.OK,
-                MessageBoxIcon.Warning);
-            return;
-        }
-
-        var barcodes = GenerateBarcodes(startIndex, endIndex, sourceCode, datePurchased);
-        var printJob = new LabelPrintJob() {
-            Labels = barcodes,
-            Copies = 1,
-            LabelSize = new LabelSize() {
-                Width = 300,
-                Height = 100
-            }
-        };
-
-        var labelPrinter = _printerFactory.GetPrinter(LabelTemplateType.OneByThree);
-        labelPrinter.Print(printJob);
-
-        MessageBox.Show("Generate!", "Information", MessageBoxButtons.OKCancel);
-    }
-
-    /// <summary>
-    /// Generates a list of rendered barcode labels based on the specified range, source code, and purchase date.
-    /// </summary>
-    /// <param name="startIndex">The starting index for generating barcode labels.</param>
-    /// <param name="endIndex">The ending index for generating barcode labels.</param>
-    /// <param name="sourceCode">The source code associated with the barcode labels. Can be <c>null</c>.</param>
-    /// <param name="datePurchased">The purchase date to be included in the barcode labels.</param>
-    /// <returns>A list of <see cref="RenderedBarcodeLabel"/> objects representing the generated barcode labels.</returns>
-    /// <remarks>
-    /// This method first generates barcode labels using the <see cref="IBarcodeLabelGenerator"/> implementation.
-    /// It then converts these labels into rendered barcode labels using the <see cref="IRenderedBarcodeLabelGenerator"/> implementation.
-    /// </remarks>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="sourceCode"/> is <c>null</c>.</exception>
-    private IList<RenderedBarcodeLabel> GenerateBarcodes(int startIndex, int endIndex, string sourceCode, DateTime datePurchased) {
-        var barcodeLabels = _barcodeLabelGenerator.Generate(startIndex, endIndex, sourceCode, datePurchased);
-        return _renderedBarcodeLabelGenerator.Generate(barcodeLabels);
-    }
-
-    /// <summary>
-    /// Initializes the numeric up-down controls with default values.
-    /// </summary>
-    /// <remarks>
-    /// This method sets the default values for the <see cref="nudStartNumber"/> and <see cref="nudEndNumber"/> controls.
-    /// The default value for <see cref="nudStartNumber"/> is set to 1, and for <see cref="nudEndNumber"/> is set to 100.
-    /// </remarks>
-    private void InitializeNumericControls() {
-        // Set default values for numeric up-down controls
-        nudStartNumber.Value = 1;
-        nudEndNumber.Value = 100;
-    }
-
-    private void label1_Click(object sender, EventArgs e) {
-        MessageBox.Show("Label clicks");
+    private void btnLaunchPriceLabelGenerator_Click(object sender, EventArgs e) {
+        var form = _pricingLabelGeneratorFormFactory();
+        form.ShowDialog(this);
     }
 
     /// <summary>
@@ -156,22 +48,5 @@ public partial class MainForm : Form {
     /// Additionally, it displays a welcome message to the user when the form is loaded.
     /// </remarks>
     private void MainForm_Load(object sender, EventArgs e) {
-        InitializeNumericControls();
-
-        PopulateSourceDropdown();
-    }
-
-    /// <summary>
-    /// Populates the source dropdown with a list of predefined sources.
-    /// </summary>
-    /// <remarks>
-    /// This method binds the <see cref="ComboBox"/> control <see cref="cmboSources"/> to a list of sources
-    /// defined in the <see cref="_sources"/> dictionary. The display member is set to the source description,
-    /// and the value member is set to the source key.
-    /// </remarks>
-    private void PopulateSourceDropdown() {
-        cmboSources.DataSource = _sources.ToList();
-        cmboSources.DisplayMember = "Value";
-        cmboSources.ValueMember = "Key";
     }
 }
