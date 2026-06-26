163269b09af91ef2670dddef493361766502ba9c
Robert <Robert.Eberhart@gmail.com>
2026-06-26 08:58:52 -0400

    Update OneByThreeLabelRenderer.cs - Removed outdated copyright information.

--- source/BarcodeGenerator.LabelGeneration/OneByThreeLabelRenderer.cs
+++ source/BarcodeGenerator.LabelGeneration/OneByThreeLabelRenderer.cs
@@ -3,10 +3,6 @@
 // Author            : Robert Eberhart
 // Created           : 06-11-2026
 // ***********************************************************************
-// <copyright file="OneByThreeLabelRenderer.cs" company="Littoral Combat Ships">
-//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
-// </copyright>
-// ***********************************************************************
 
 using BarcodeGenerator.Entities;
 using System.Runtime.Versioning;
