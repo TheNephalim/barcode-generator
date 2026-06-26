29c2db86e9b64a6f1b06209b52ba860beb893a57
Robert <Robert.Eberhart@gmail.com>
2026-06-26 08:58:11 -0400

    Update BarcodeLabelGenerator.cs - Removed outdated copyright information.

--- source/BarcodeGenerator.LabelGeneration/BarcodeLabelGenerator.cs
+++ source/BarcodeGenerator.LabelGeneration/BarcodeLabelGenerator.cs
@@ -3,10 +3,6 @@
 // Author            : Robert Eberhart
 // Created           : 06-10-2026
 // ***********************************************************************
-// <copyright file="BarcodeLabelGenerator.cs" company="Littoral Combat Ships">
-//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
-// </copyright>
-// ***********************************************************************
 
 using BarcodeGenerator.Entities;
 
