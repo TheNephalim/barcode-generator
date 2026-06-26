cae164a8c0610f4dd56213097e7f6d66d55abf5a
Robert <Robert.Eberhart@gmail.com>
2026-06-26 08:59:02 -0400

    Update FormRegistrar.cs - Removed outdated copyright information.

--- source/BarcodeGenerator/FormRegistrar.cs
+++ source/BarcodeGenerator/FormRegistrar.cs
@@ -3,10 +3,6 @@
 // Author            : Robert Eberhart
 // Created           : 06-10-2026
 // ***********************************************************************
-// <copyright file="FormRegistrar.cs" company="Littoral Combat Ships">
-//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
-// </copyright>
-// ***********************************************************************
 
 using Autofac;
 
