59bc8d8eb12dfd673ec4fd981e675cf677c70625
Robert <Robert.Eberhart@gmail.com>
2026-07-03 09:48:51 -0400

    Add new Repositories folder to BarcodeGenerator.Data for repository pattern implementation

---
+++ source/BarcodeGenerator.Data/Repositories/IInventorySourceRepository.cs
@@ -0,0 +1,35 @@
+﻿// ***********************************************************************
+// Assembly          : ${$NAMESPACE$}
+// Author            : Robert Eberhart
+// Created           : 07-03-2026
+// ***********************************************************************
+// <copyright file="IInventorySourceRepository.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using BarcodeGenerator.Entities;
+
+namespace BarcodeGenerator.Data.Repositories;
+
+/// <summary>
+/// Defines a repository interface for managing and retrieving inventory source data.
+/// </summary>
+/// <remarks>
+/// This interface is part of the <c>BarcodeGenerator.Data.Repositories</c> namespace and provides
+/// methods for interacting with inventory source entities stored in the database.
+/// </remarks>
+public interface IInventorySourceRepository {
+
+    /// <summary>
+    /// Retrieves all inventory sources from the database.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains a read-only list of
+    /// <see cref="InventorySource"/> objects, ordered by their names in ascending order.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if there is an issue with the database connection or query execution.
+    /// </exception>
+    Task<IReadOnlyList<InventorySource>> GetAllAsync();
+}
---
+++ source/BarcodeGenerator.Data/Repositories/InventorySourceRepository.cs
@@ -0,0 +1,54 @@
+﻿// ***********************************************************************
+// Assembly          : BarcodeGenerator.Data.Repositories
+// Author            : Robert Eberhart
+// Created           : 07-03-2026
+// ***********************************************************************
+// <copyright file="InventorySourceRepository.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using BarcodeGenerator.Data.Database;
+using BarcodeGenerator.Entities;
+using Dapper;
+
+namespace BarcodeGenerator.Data.Repositories;
+
+public class InventorySourceRepository : IInventorySourceRepository {
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InventorySourceRepository"/> class.
+    /// </summary>
+    /// <param name="dbConnectionFactory">
+    /// An instance of <see cref="IDbConnectionFactory"/> used to create database connections.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="dbConnectionFactory"/> is <c>null</c>.
+    /// </exception>
+    public InventorySourceRepository(IDbConnectionFactory dbConnectionFactory) {
+        _dbConnectionFactory = dbConnectionFactory ?? throw new ArgumentNullException(nameof(dbConnectionFactory));
+    }
+
+    /// <summary>
+    /// Retrieves all inventory sources from the database.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains a read-only list of
+    /// <see cref="InventorySource"/> objects, ordered by their names in ascending order.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if there is an issue with the database connection or query execution.
+    /// </exception>
+    public async Task<IReadOnlyList<InventorySource>> GetAllAsync() {
+        const string sql = """
+                           SELECT Id, Code, Name, IsActive
+                           FROM InventorySource
+                           ORDER BY NAME ASC
+                           """;
+
+        using var connection = _dbConnectionFactory.CreateConnection();
+        var result = await connection.QueryAsync<InventorySource>(sql);
+        return result.ToList().AsReadOnly();
+    }
+}
