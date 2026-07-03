b3469d315a73af02efc585d1d99eadb38a91beab
Robert <Robert.Eberhart@gmail.com>
2026-07-03 09:49:11 -0400

    Add PurchaseLot.cs to BarcodeGenerator.Entities for managing purchase lot information

---
+++ source/BarcodeGenerator.Entities/PurchaseLot.cs
@@ -0,0 +1,19 @@
+﻿// ***********************************************************************
+// Assembly          : BarcodeGenerator.Entities
+// Author            : Robert Eberhart
+// Created           : 07-03-2026
+// ***********************************************************************
+// <copyright file="PurchaseLot.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+namespace BarcodeGenerator.Entities;
+
+public class PurchaseLot {
+    public string Description { get; set; }
+    public string EstimatedItemCount { get; set; }
+    public int Id { get; set; }
+    public string LotDate { get; set; }
+    public string SourceCode { get; set; }
+    public decimal TotalCost { get; set; }
+}
