bd6857c5d3cf3622a17533dd8afd5b8af500a2ce
Robert <Robert.Eberhart@gmail.com>
2026-07-05 11:32:52 -0400

    Clean up InventorySourceRepository: - Removed redundant copyright comments.

--- source/BarcodeGenerator.Data/Repositories/InventorySourceRepository.cs
+++ source/BarcodeGenerator.Data/Repositories/InventorySourceRepository.cs
@@ -3,10 +3,6 @@
 // Author            : Robert Eberhart
 // Created           : 07-03-2026
 // ***********************************************************************
-// <copyright file="InventorySourceRepository.cs" company="Littoral Combat Ships">
-//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
-// </copyright>
-// ***********************************************************************
 
 using BarcodeGenerator.Data.Database;
 using BarcodeGenerator.Entities;
