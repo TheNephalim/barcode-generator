0648d511229eb82aba657b38e17d873d4f5485b1
Robert <Robert.Eberhart@gmail.com>
2026-07-26 16:33:25 -0400

    Refactor PriceLabelGenerator: - Simplified namespace declaration and improved code structure.

--- source/BarcodeGenerator/PriceLabelGenerator.cs
+++ source/BarcodeGenerator/PriceLabelGenerator.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace BarcodeGenerator;
 
-namespace BarcodeGenerator {
-    public partial class PriceLabelGenerator : Form {
-        public PriceLabelGenerator() {
-            InitializeComponent();
-        }
+public partial class PriceLabelGenerator : Form {
+
+    public PriceLabelGenerator() {
+        InitializeComponent();
     }
 }
