37ad43cbe9b0a28f767442c79a0f260e001e41bd
Robert <Robert.Eberhart@gmail.com>
2026-07-26 16:31:57 -0400

    Implement new repository methods in InventorySourceRepository: - AddSourceAsync: Inserts new inventory sources into the database. - UpdateSourceAsync: Updates existing inventory sources. - Enhanced GetAllAsync to include new columns.

--- source/BarcodeGenerator.Data/Repositories/InventorySourceRepository.cs
+++ source/BarcodeGenerator.Data/Repositories/InventorySourceRepository.cs
@@ -27,6 +27,31 @@ public class InventorySourceRepository : IInventorySourceRepository {
     }
 
     /// <summary>
+    /// Adds a new inventory source to the database.
+    /// </summary>
+    /// <param name="source">
+    /// An instance of <see cref="InventorySource"/> representing the inventory source to be added.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Task"/> representing the asynchronous operation.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when the operation does not affect exactly one row in the database.
+    /// </exception>
+    public async Task AddSourceAsync(InventorySource source) {
+        const string sql = """
+                           INSERT INTO InventorySource(Code, Name, IsActive, LastPrintedNumber, LastPurchaseDate)
+                           VALUES(@Code, @Name, @IsActive, @LastPrintedNumber, @LastPurchaseDate);
+                           """;
+        using var connection = _dbConnectionFactory.CreateConnection();
+        var rowsAffected = await connection.ExecuteAsync(sql, source);
+
+        if (rowsAffected != 1) {
+            throw new InvalidOperationException($"Expected to add one inventory source, but updated {rowsAffected}");
+        }
+    }
+
+    /// <summary>
     /// Retrieves all inventory sources from the database.
     /// </summary>
     /// <returns>
@@ -38,7 +63,7 @@ public class InventorySourceRepository : IInventorySourceRepository {
     /// </exception>
     public async Task<IReadOnlyList<InventorySource>> GetAllAsync() {
         const string sql = """
-                           SELECT Id, Code, Name, IsActive
+                           SELECT Id, Code, Name, IsActive, LastPrintedNumber, LastPurchaseDate
                            FROM InventorySource
                            ORDER BY NAME ASC
                            """;
@@ -46,5 +71,67 @@ public class InventorySourceRepository : IInventorySourceRepository {
         using var connection = _dbConnectionFactory.CreateConnection();
         var result = await connection.QueryAsync<InventorySource>(sql);
         return result.ToList().AsReadOnly();
+    }
+
+    /// <summary>
+    /// Retrieves an <see cref="InventorySource"/> entity by its unique code.
+    /// </summary>
+    /// <param name="code">
+    /// The unique code of the inventory source to retrieve.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the
+    /// <see cref="InventorySource"/> entity associated with the specified code.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when no inventory source is found for the specified code.
+    /// </exception>
+    /// <remarks>
+    /// This method queries the database for an inventory source with the given code.
+    /// If no matching entity is found, an exception is thrown.
+    /// </remarks>
+    public async Task<InventorySource> GetById(string code) {
+        const string sql = """
+                           SELECT Id, Code, Name, IsActive, LastPrintedNumber, LastPurchaseDate
+                           FROM InventorySource
+                           Where Code = @code
+                           """;
+
+        using var connection = _dbConnectionFactory.CreateConnection();
+        var result = await connection.ExecuteScalarAsync<InventorySource>(sql, code);
+
+        return result ?? throw new InvalidOperationException($"Expected to update one inventory source.");
+    }
+
+    /// <summary>
+    /// Updates the specified inventory source in the database.
+    /// </summary>
+    /// <param name="source">
+    /// An instance of <see cref="InventorySource"/> containing the updated data for the inventory source.
+    /// </param>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when the update operation does not affect exactly one record in the database.
+    /// </exception>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// </returns>
+    public async Task UpdateSourceAsync(InventorySource source) {
+        const string sql = """
+                           UPDATE InventorySource
+                           SET
+                            Code = @Code,
+                            Name = @Name,
+                            IsActive = @IsActive,
+                            LastPrintedNumber = @LastPrintedNumber,
+                            LastPurchaseDate = @LastPurchaseDate
+                           WHERE ID = @Id;
+                           """;
+
+        using var connection = _dbConnectionFactory.CreateConnection();
+        var rowsAffected = await connection.ExecuteAsync(sql, source);
+
+        if (rowsAffected != 1) {
+            throw new InvalidOperationException($"Expected to update one inventory source, but updated {rowsAffected}");
+        }
     }
 }
