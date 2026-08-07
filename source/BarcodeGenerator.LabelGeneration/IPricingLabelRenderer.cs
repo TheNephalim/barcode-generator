7aab811f93df9ad299f5be282e1a3f61f7bab907
Robert <Robert.Eberhart@gmail.com>
2026-08-07 18:24:20 -0400

    Add IPricingLabelRenderer interface: - Defines rendering logic for pricing labels.

---
+++ source/BarcodeGenerator.LabelGeneration/IPricingLabelRenderer.cs
@@ -0,0 +1,13 @@
+﻿// ***********************************************************************
+// Assembly          : BarcodeGenerator.LabelGeneration
+// Author            : Robert Eberhart
+// Created           : 08-04-2026
+// ***********************************************************************
+// <copyright file="IPricingLabelRenderer.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+namespace BarcodeGenerator.LabelGeneration;
+
+public sealed class IPricingLabelRenderer {
+}
