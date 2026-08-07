65127a566b85306414a383243032b71b6b285aad
Robert <Robert.Eberhart@gmail.com>
2026-08-07 18:24:09 -0400

    Add IPrintableLabel interface: - Defines a contract for printable label entities.

---
+++ source/BarcodeGenerator.Entities/IPrintableLabel.cs
@@ -0,0 +1,20 @@
+﻿// ***********************************************************************
+// Assembly          : ${$NAMESPACE$}
+// Author            : Robert Eberhart
+// Created           : 08-06-2026
+// ***********************************************************************
+// <copyright file="IPrintableLabel.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+namespace BarcodeGenerator.Entities;
+
+/// <summary>
+/// Represents a contract for a printable label within the barcode generation system.
+/// </summary>
+/// <remarks>
+/// This interface is part of the <c>BarcodeGenerator.LabelGeneration</c> namespace and is designed to define
+/// the structure and behavior required for labels that can be printed.
+/// </remarks>
+public interface IPrintableLabel;
