1e6e3a2c2d296dac764f639a353fa76eb70a73fb
Robert <Robert.Eberhart@gmail.com>
2026-09-04 16:39:21 -0400

    Add InventoryLabelRenderer: - Implements rendering logic for inventory labels. Reason: Support new label rendering requirements.

---
+++ source/BarcodeGenerator.LabelGeneration/InventoryLabelRenderer.cs
@@ -0,0 +1,23 @@
+﻿// ***********************************************************************
+// Assembly          : BarcodeGenerator.LabelGeneration
+// Author            : Robert Eberhart
+// Created           : 09-04-2026
+// ***********************************************************************
+// <copyright file="InventoryLabelRenderer.cs" company="Littoral Combat Ships">
+//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using BarcodeGenerator.Entities;
+
+namespace BarcodeGenerator.LabelGeneration;
+
+public sealed class InventoryLabelRenderer : ILabelRenderer {
+    public LabelTemplateType TemplateType => LabelTemplateType.Inventory;
+
+    public void Render(IPrintableLabel label, Graphics graphics, Rectangle bounds) {
+        if (label is not RenderedInventoryLabel inventoryLabel) {
+            throw new ArgumentException($"Expected {nameof(RenderedInventoryLabel)}", nameof(label));
+        }
+    }
+}
