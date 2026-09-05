aa6afe94b192b51d99258ad0468c3665b882b912
Robert <Robert.Eberhart@gmail.com>
2026-09-05 08:09:30 -0400

    Remove redundant copyright headers from RenderedInventoryLabel.cs - Simplified the file by removing unnecessary metadata.

--- source/BarcodeGenerator.Entities/RenderedInventoryLabel.cs
+++ source/BarcodeGenerator.Entities/RenderedInventoryLabel.cs
@@ -3,10 +3,6 @@
 // Author            : Robert Eberhart
 // Created           : 09-04-2026
 // ***********************************************************************
-// <copyright file="RenderedInventoryLabel.cs" company="Littoral Combat Ships">
-//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
-// </copyright>
-// ***********************************************************************
 using System.Drawing;
 
 namespace BarcodeGenerator.Entities;
