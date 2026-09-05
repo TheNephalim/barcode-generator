bfedf94f32b3222f98cbc839225160405cadc091
Robert <Robert.Eberhart@gmail.com>
2026-09-05 08:09:28 -0400

    Remove redundant copyright headers from PricingLabel.cs - Simplified the file by removing unnecessary metadata.

--- source/BarcodeGenerator.Entities/PricingLabel.cs
+++ source/BarcodeGenerator.Entities/PricingLabel.cs
@@ -3,10 +3,6 @@
 // Author            : Robert Eberhart
 // Created           : 08-04-2026
 // ***********************************************************************
-// <copyright file="PricingLabel.cs" company="Littoral Combat Ships">
-//     Copyright (c) 2026 Littoral Combat Ships. All rights reserved.
-// </copyright>
-// ***********************************************************************
 namespace BarcodeGenerator.Entities;
 
 /// <summary>
